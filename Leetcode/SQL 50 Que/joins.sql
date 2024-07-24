f077f52cb53593cd04ebcacd44fe77394487051d
GillKaran23 <gillkaran2352@gmail.com>
2024-07-24 08:10:49 +0530

    Update joins

--- Leetcode/SQL 50 Que/joins.sql
+++ Leetcode/SQL 50 Que/joins.sql
@@ -72,3 +72,52 @@ SELECT B.product_name, A.year, A.price FROM Sales AS A INNER JOIN Product AS B O
 -- transaction_id is column with unique values for this table.
 -- This table contains information about the transactions made during the visit_id.
 -- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+SELECT A.customer_id, COUNT(*) AS count_no_trans FROM Visits A
+LEFT JOIN Transactions B ON A.visit_id = B.visit_id
+WHERE B.transaction_id IS NULL
+GROUP BY A.customer_id;
+
+
+
+-- 4 Question
+-- Table: Weather
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the column with unique values for this table.
+-- There are no different rows with the same recordDate.
+-- This table contains information about the temperature on a certain day.
+-- Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
+
+
+
+-- 5 Question
+-- Table: Activity
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | machine_id     | int     |
+-- | process_id     | int     |
+-- | activity_type  | enum    |
+-- | timestamp      | float   |
+-- +----------------+---------+
+-- The table shows the user activities for a factory website.
+-- (machine_id, process_id, activity_type) is the primary key (combination of columns with unique values) of this table.
+-- machine_id is the ID of a machine.
+-- process_id is the ID of a process running on the machine with ID machine_id.
+-- activity_type is an ENUM (category) of type ('start', 'end').
+-- timestamp is a float representing the current time in seconds.
+-- 'start' means the machine starts the process at the given timestamp and 'end' means the machine ends the process at the given timestamp.
+-- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair
+-- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+-- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+-- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.

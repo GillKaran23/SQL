e9ec2f263c8128b35411f02492885d7e66db809c
GillKaran23 <gillkaran2352@gmail.com>
2024-07-25 17:20:39 +0530

    Today's work

--- HackerRank/SQL/Easy/select.sql
+++ HackerRank/SQL/Easy/select.sql
@@ -190,3 +190,5 @@ SELECT TRUNCATE(SUM(LAT_N), 4) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 13
 SELECT TRUNCATE(MAX(LAT_N),4) FROM STATION WHERE LAT_N<137.2345;
 
 
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N = ( SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);

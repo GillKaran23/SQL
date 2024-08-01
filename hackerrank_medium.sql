f2e792aec0bf9b7df65b7aef7da9baedcd9384e1
GillKaran23 <gillkaran2352@gmail.com>
2024-08-01 18:39:39 +0530

    SQL

--- hackerrank_medium.sql
+++ hackerrank_medium.sql
@@ -47,3 +47,19 @@ SELECT b.N,
         END)
 FROM bst b 
 ORDER BY N;
+
+
+-- You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+-- Grades contains the following data:
+-- Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+SELECT
+  CASE
+    WHEN B.grade < 8 THEN NULL
+    ELSE A.name
+  END,
+  B.grade,
+  A.marks
+FROM students AS A
+INNER JOIN grades AS B
+ON A.marks >= B.min_mark AND A.marks <= B.max_mark
+ORDER BY B.grade DESC, A.name ASC;

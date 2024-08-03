fb964b5ab72c8d0ad3f34a5968a7b2f727a4e06c
GillKaran23 <gillkaran2352@gmail.com>
2024-08-03 09:02:25 +0530

    SQL

--- leetcode_easy.sql
+++ leetcode_easy.sql
@@ -525,3 +525,17 @@ GROUP BY
     activity_date
 ORDER BY 
     activity_date;
+
+
+-- Table: Logins
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+-- Each row contains information about the login time for the user with ID user_id.
+-- Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.
+SELECT user_id, MAX(time_stamp) AS last_stamp FROM Logins WHERE time_stamp LIKE '2020%' GROUP BY user_id;
+
--- leetcode_joins.sql
+++ leetcode_joins.sql
@@ -171,3 +171,50 @@ FROM Employee AS A
 LEFT JOIN Bonus AS B 
 ON A.empId = B.empId 
 WHERE B.empId IS NULL OR B.bonus < 1000;
+
+
+-- 7 Question
+-- Table: Students
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | student_id    | int     |
+-- | student_name  | varchar |
+-- +---------------+---------+
+-- student_id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the ID and the name of one student in the school.
+-- Table: Subjects
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | subject_name | varchar |
+-- +--------------+---------+
+-- subject_name is the primary key (column with unique values) for this table.
+-- Each row of this table contains the name of one subject in the school.
+-- Table: Examinations
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | student_id   | int     |
+-- | subject_name | varchar |
+-- +--------------+---------+
+-- There is no primary key (column with unique values) for this table. It may contain duplicates.
+-- Each student from the Students table takes every course from the Subjects table.
+-- Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+SELECT A.student_id, 
+       A.student_name, 
+       B.subject_name, 
+       Count(C.subject_name) AS attended_exams 
+FROM   Students AS A 
+       CROSS JOIN Subjects AS B 
+       LEFT JOIN Examinations AS C 
+              ON A.student_id = C.student_id 
+                 AND B.subject_name = C.subject_name 
+GROUP  BY A.student_id, 
+          B.subject_name
+ORDER  BY A.student_id, 
+          B.subject_name;
+
+

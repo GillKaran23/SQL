bc20557c5a90bf8bb17609777093771924ef2134
GillKaran23 <gillkaran2352@gmail.com>
2024-08-05 09:52:40 +0530

    SQL

--- leetcode_easy.sql
+++ leetcode_easy.sql
@@ -560,3 +560,51 @@ SELECT date_id,
 FROM DailySales
 GROUP BY date_id, make_name
 ORDER BY NULL;
+
+
+-- Table: Products
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_id  | int     |
+-- | store1      | int     |
+-- | store2      | int     |
+-- | store3      | int     |
+-- +-------------+---------+
+-- product_id is the primary key (column with unique values) for this table.
+-- Each row in this table indicates the product's price in 3 different stores: store1, store2, and store3.
+-- If the product is not available in a store, the price will be null in that store's column.
+-- Write a solution to rearrange the Products table so that each row has (product_id, store, price). If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
+SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL
+UNION ALL
+SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL
+UNION ALL
+SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL;
+
+
+-- Table: Employees
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | employee_id | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- employee_id is the column with unique values for this table.
+-- Each row of this table indicates the name of the employee whose ID is employee_id.
+-- Table: Salaries
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | employee_id | int     |
+-- | salary      | int     |
+-- +-------------+---------+
+-- employee_id is the column with unique values for this table.
+-- Each row of this table indicates the salary of the employee whose ID is employee_id.
+-- Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+-- The employee's name is missing, or
+-- The employee's salary is missing.
+-- Return the result table ordered by employee_id in ascending order.
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION ALL
+SELECT employee_id FROM Salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;

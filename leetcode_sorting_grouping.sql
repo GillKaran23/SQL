fdac7a6d153e93e78bb5a040a73ad110ef9d1807
GillKaran23 <gillkaran2352@gmail.com>
2024-08-08 18:23:44 +0530

    SQL

--- leetcode_sorting_grouping.sql
+++ leetcode_sorting_grouping.sql
@@ -139,3 +139,24 @@ SELECT
     END AS num;
 
 
+-- 7 Question
+-- Table: Customer
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | customer_id | int     |
+-- | product_key | int     |
+-- +-------------+---------+
+-- This table may contain duplicates rows. 
+-- customer_id is not NULL.
+-- product_key is a foreign key (reference column) to Product table.
+-- Table: Product
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | product_key | int     |
+-- +-------------+---------+
+-- product_key is the primary key (column with unique values) for this table.
+-- Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+-- Return the result table in any order.
+SELECT customer_id FROM customer GROUP BY customer_id HAVING Count(DISTINCT product_key) = (SELECT Count(DISTINCT product_key) AS total_product FROM product);

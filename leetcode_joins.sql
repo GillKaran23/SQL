2137aed0c93aa360202350e2d533c004f5c42d31
GillKaran23 <gillkaran2352@gmail.com>
2024-08-10 17:37:05 +0530

    SQL

--- leetcode_joins.sql
+++ leetcode_joins.sql
@@ -218,3 +218,19 @@ ORDER  BY A.student_id,
           B.subject_name;
 
 
+-- 8 Question
+-- Table: Employee
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+-- Write a solution to find managers with at least five direct reports.
+SELECT A.name FROM Employee AS A INNER JOIN EMPLOYEE AS B ON A.id=B.managerId GROUP BY A.id,A.name HAVING COUNT(B.managerID)>= 5;

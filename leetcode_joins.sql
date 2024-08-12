84dba683c3259171ce57bc99f7dd66caa4433151
GillKaran23 <gillkaran2352@gmail.com>
2024-08-12 16:39:50 +0530

    SQL

--- leetcode_joins.sql
+++ leetcode_joins.sql
@@ -234,3 +234,38 @@ ORDER  BY A.student_id,
 -- No employee will be the manager of themself.
 -- Write a solution to find managers with at least five direct reports.
 SELECT A.name FROM Employee AS A INNER JOIN EMPLOYEE AS B ON A.id=B.managerId GROUP BY A.id,A.name HAVING COUNT(B.managerID)>= 5;
+
+
+-- 9 Question
+-- Table: Signups
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- user_id is the column of unique values for this table.
+-- Each row contains information about the signup time for the user with ID user_id.
+-- Table: Confirmations
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- | action         | ENUM     |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+-- user_id is a foreign key (reference column) to the Signups table.
+-- action is an ENUM (category) of the type ('confirmed', 'timeout')
+-- Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+-- Write a solution to find the confirmation rate of each user.
+SELECT
+  A.user_id,
+  ROUND((SUM(IF(B.action = "confirmed", 1, 0)) / COUNT(*)), 2) AS confirmation_rate
+FROM
+  Signups A
+LEFT JOIN
+  Confirmations B ON A.user_id = B.user_id
+GROUP BY
+  A.user_id;

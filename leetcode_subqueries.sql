06fe24e2c7868a019554f6cdcc921098f38b6b2f
GillKaran23 <gillkaran2352@gmail.com>
2024-08-13 18:25:30 +0530

    SQL

--- leetcode_subqueries.sql
+++ leetcode_subqueries.sql
@@ -15,6 +15,29 @@
 SELECT employee_id FROM Employees WHERE manager_id NOT IN(SELECT employee_id FROM Employees) AND salary<30000 ORDER BY employee_id;
 
 
+-- 2 Question
+-- Table: Seat
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | student     | varchar |
+-- +-------------+---------+
+-- id is the primary key (unique value) column for this table.
+-- Each row of this table indicates the name and the ID of a student.
+-- id is a continuous increment.
+-- Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+-- Return the result table ordered by id in ascending order.
+SELECT ( CASE
+            WHEN id%2 != 0 AND id != counts THEN id+1
+            WHEN id%2 != 0 AND id = counts THEN id
+            ELSE id-1
+        END) AS id, student
+FROM seat, (select count(*) as counts from seat) 
+AS seat_counts
+ORDER BY id ASC
+
+
 -- 3 Question
 -- Table: Movies
 -- +---------------+---------+

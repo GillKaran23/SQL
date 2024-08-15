4a43ca8f8e51a2bb859a08186739b8afa51e2a01
GillKaran23 <gillkaran2352@gmail.com>
2024-08-15 07:02:03 +0530

    SQL

--- leetcode_aggregation.sql
+++ leetcode_aggregation.sql
@@ -174,3 +174,28 @@ FROM (
   FROM delivery
   GROUP BY customer_id
 ) B;
+
+
+8 Question
+-- Table: Activity
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | player_id    | int     |
+-- | device_id    | int     |
+-- | event_date   | date    |
+-- | games_played | int     |
+-- +--------------+---------+
+-- (player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+-- This table shows the activity of players of some games.
+-- Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+-- Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+SELECT 
+ROUND((SELECT COUNT(DISTINCT A.player_id) from Activity A
+INNER JOIN 
+(SELECT player_id, MIN(event_date) AS first_logged 
+    FROM Activity 
+    GROUP BY player_id) B ON DATEDIFF(A.event_date, B.first_logged)=1
+    AND A.player_id = B.player_id)
+    /
+    (SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction;

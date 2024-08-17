58a976075150424dae17c9298b41f0a4afdaede3
GillKaran23 <gillkaran2352@gmail.com>
2024-08-17 06:53:10 +0530

    SQL

--- leetcode_advance-select.sql
+++ leetcode_advance-select.sql
@@ -119,6 +119,34 @@ GROUP BY product_id
 HAVING MIN(change_date)> '2019-08-16'
 
 
+-- 6 Question
+-- Table: Queue
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | person_id   | int     |
+-- | person_name | varchar |
+-- | weight      | int     |
+-- | turn        | int     |
+-- +-------------+---------+
+-- person_id column contains unique values.
+-- This table has the information about all people waiting for a bus.
+-- The person_id and turn columns will contain all numbers from 1 to n, where n is the number of rows in the table.
+-- turn determines the order of which the people will board the bus, where turn=1 denotes the first person to board and turn=n denotes the last person to board.
+-- weight is the weight of the person in kilograms.
+-- There is a queue of people waiting to board a bus. However, the bus has a weight limit of 1000 kilograms, so there may be some people who cannot board.
+-- Write a solution to find the person_name of the last person that can fit on the bus without exceeding the weight limit. The test cases are generated such that the first person does not exceed the weight limit.
+SELECT A.person_name
+FROM
+    Queue AS A,
+    Queue AS B
+WHERE A.turn >= B.turn
+GROUP BY A.person_id
+HAVING SUM(B.weight) <= 1000
+ORDER BY A.turn DESC
+LIMIT 1;
+
+
 -- 7 Question
 -- Table: Accounts
 -- +-------------+------+

0bd3c7b15126534a80de4c2c2fcc954b68c3d740
GillKaran23 <gillkaran2352@gmail.com>
2024-08-22 18:04:33 +0530

    SQL

--- leetcode_medium.sql
+++ leetcode_medium.sql
@@ -145,3 +145,32 @@ SELECT stock_name,
        SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name;
+
+
+-- Table: Employee
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+-- +--------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- departmentId is a foreign key (reference columns) of the ID from the Department table.
+-- Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+-- Table: Department
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+-- Each row of this table indicates the ID of a department and its name.
+-- Write a solution to find employees who have the highest salary in each of the departments.
+SELECT A.name AS Department, B.name AS Employee, salary AS Salary FROM Department A 
+JOIN Employee B
+ON B.departmentId = A.id
+WHERE (B.Salary, B.DepartmentId) IN (SELECT MAX(B.Salary), B.DepartmentId 
+FROM Employee B GROUP BY B.DepartmentId);

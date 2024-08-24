6795d2f38ed7744ed5ef871a35271330b3c02050
GillKaran23 <gillkaran2352@gmail.com>
2024-08-24 13:39:44 +0530

    SQL

--- leetcode_medium.sql
+++ leetcode_medium.sql
@@ -198,3 +198,24 @@ WHEN p_id IS NULL THEN 'Root'
 WHEN p_id IS NOT NULL AND id IN (SELECT DISTINCT p_id FROM tree) THEN 'Inner'
 ELSE 'Leaf' END AS Type
 FROM tree;
+
+
+-- Table: transactions
+-- +------------------+------+
+-- | Column Name      | Type | 
+-- +------------------+------+
+-- | transaction_id   | int  |
+-- | amount           | int  |
+-- | transaction_date | date |
+-- +------------------+------+
+-- The transactions_id column uniquely identifies each row in this table.
+-- Each row of this table contains the transaction id, amount and transaction date.
+-- Write a solution to find the sum of amounts for odd and even transactions for each day. If there are no odd or even transactions for a specific date, display as 0.
+-- Return the result table ordered by transaction_date in ascending order.
+SELECT
+    transaction_date,
+    SUM(IF(amount % 2 = 1, amount, 0)) AS odd_sum,
+    SUM(IF(amount % 2 = 0, amount, 0)) AS even_sum
+FROM transactions
+GROUP BY transaction_date
+ORDER BY transaction_date;

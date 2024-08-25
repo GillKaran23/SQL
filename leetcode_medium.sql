975e19c6a271c9ab38ee37710a25f29ff510bbcf
GillKaran23 <gillkaran2352@gmail.com>
2024-08-25 09:02:25 +0530

    SQL

--- leetcode_medium.sql
+++ leetcode_medium.sql
@@ -219,3 +219,45 @@ SELECT
 FROM transactions
 GROUP BY transaction_date
 ORDER BY transaction_date;
+
+
+-- Table: Users
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | user_id        | int     |
+-- | join_date      | date    |
+-- | favorite_brand | varchar |
+-- +----------------+---------+
+-- user_id is the primary key (column with unique values) of this table.
+-- This table has the info of the users of an online shopping website where users can sell and buy items.
+-- Table: Orders
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | order_id      | int     |
+-- | order_date    | date    |
+-- | item_id       | int     |
+-- | buyer_id      | int     |
+-- | seller_id     | int     |
+-- +---------------+---------+
+-- order_id is the primary key (column with unique values) of this table.
+-- item_id is a foreign key (reference column) to the Items table.
+-- buyer_id and seller_id are foreign keys to the Users table.
+-- Table: Items
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | item_id       | int     |
+-- | item_brand    | varchar |
+-- +---------------+---------+
+-- item_id is the primary key (column with unique values) of this table.
+-- Write a solution to find for each user, the join date and the number of orders they made as a buyer in 2019.
+SELECT A.user_id AS buyer_id,
+       A.join_date,
+       count(B.order_id) AS orders_in_2019
+FROM Users AS A
+LEFT JOIN Orders B ON A.user_id = B.buyer_id
+    AND B.order_date LIKE '%2019%'
+GROUP BY A.user_id, A.join_date
+ORDER BY A.user_id;
